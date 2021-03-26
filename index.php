9ac818b2f4652a0cb855b57104715f8bb34f6158
abk26 <80499950+abk26@users.noreply.github.com>
2021-03-26 09:58:53 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Portfolio
+	</title>
+</head>
+<body>
+	<p>Name :Aibek</p>
+	<p>LastName : kaldybaev</p>
+	<p>University : Ala-Too</p>
+	
+	
+</body>
+</html>
